2e07ba9ac1b82c9ff4d535005212f932a76e9ef8
martynawieruszewska <martyna.wieruszewska@gmail.com>
2025-03-31 12:04:32 +0000

    year selection

--- TrendChecker.java
+++ TrendChecker.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class TrendChecker {
     public static void main(String[] args) {
+        int choice; 
+
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to my fahsion trends project!");
+        System.out.println("Pick a year to unveil the most notable seasonal trends: ");
+        System.out.println("(1) 2024");
+        System.out.println("(2) 2025");
+
+        choice = input.nextInt();
+        switch(choice) {
+            case 1:
+            System.out.println("You've selected 2024.");
+            case 2:
+            System.out.println("You've selected 2025.");
+            default:
+            System.out.println("Invalid choide, try again!");
+        }
+        input.close();
     }
 }
